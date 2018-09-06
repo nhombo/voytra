1f7984faf133102500feba7110b5cd864b782ee1
nhombo <formawork@gmail.com>
2018-09-06 11:04:05 +0200

    commit pour voytrasite

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var path = require('path');
+module.exports = {
+  entry: {
+    App: "./app/assets/scripts/App.js",
+    Vendor: "./app/assets/scripts/Vendor.js"
+  },
+  output: {
+    path: path.resolve(__dirname, "./app/temp/scripts"),
+    filename: "[name].js"
+  },
+
+  module: {
+  rules: [
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2015']
+        }
+      }
+    }
+  ]
+}
+  /*
+  module:{
+    loaders:[
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        },
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  }
+*/
+
+}
